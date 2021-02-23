9ce82e5106672e9c54479aef1b4cba4c915f2eba
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-02-23 15:00:28 +0300

    fix(hello.js): add second task

---
+++ secondTask/hello.js
@@ -0,0 +1,47 @@
+'use strict';
+// Задаем текущую дату, относительно которой будем получать день недели, время, время суток и время до нового года
+const date = new Date();
+
+// Создаем функцию, позволяющую выводить на экран нужный день недели
+function getWeekDay(date) {
+  const weekDays = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+  return weekDays[date.getDay()];
+}
+
+// Рассчитаем время до нового года
+function getNewYear(newYear) {
+  const dayNow = date.getTime(),
+    dayNewYear = new Date(newYear),
+    howNewYear = Math.ceil((dayNewYear - dayNow) / 1000 / 60 / 60 / 24);
+  return howNewYear;
+}
+getNewYear('1 january 2022 00:00:00');
+
+// Определяем время суток (6-11 - Утро, 11-16 - День, 16-21 - Вечер, 21-6 - Ночь)
+function getTimeOfDay() {
+  if (date.getHours() >= 6 && date.getHours() <= 11) {
+    const timeDay = 'Доброе утро!';
+    return timeDay;
+  }
+  if (date.getHours() > 11 && date.getHours() <= 16) {
+    const timeDay = 'Добрый день!';
+    return timeDay;
+  }
+  if (date.getHours() > 16 && date.getHours() <= 21) {
+    const timeDay = 'Добрый вечер!';
+    return timeDay;
+  }
+  if (date.getHours() > 21 && date.getHours() < 6) {
+    const timeDay = 'Доброй ночи!';
+    return timeDay;
+  }
+}
+getTimeOfDay();
+
+// Вывод на экран
+document.querySelector('.hello').innerHTML = (`
+${getTimeOfDay()} <br/><br/>
+Сегодня: ${getWeekDay(date)} <br/><br/>
+Текущее время: ${date.toLocaleTimeString('en')} <br/><br/>
+До нового года осталось ${getNewYear('1 january 2022 00:00:00')} дней
+`);
