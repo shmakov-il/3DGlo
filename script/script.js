ccb6c8a9f6cc14c855705268384ac741a3169549
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-02-25 16:40:33 +0300

    fix(script.js): add animation

--- script/script.js
+++ script/script.js
@@ -28,7 +28,9 @@ window.addEventListener('DOMContentLoaded', () => {
         seconds: addZero(seconds)
       };
     };
+    let updateClockAnimation;
     const updateClock = () => {
+      updateClockAnimation = requestAnimationFrame(updateClock);
       const timer = getTimeRemaining();
       if (timer.timeRemaining > 0) {
         timerHours.textContent = timer.hours;
@@ -42,54 +44,47 @@ window.addEventListener('DOMContentLoaded', () => {
         timerSeconds.textContent = '00';
       }
     };
-    setInterval(updateClock, 1000);
-    updateClock();
+    updateClockAnimation = requestAnimationFrame(updateClock);
   };
-  countTimer('26 february 2021 20:52:10');
+  countTimer('29 february 2021 20:52:10');
 
   // Menu
   const toggleMenu = () => {
     const menu = document.querySelector('menu');
-
     const handlerMenu = () => {
       menu.classList.toggle('active-menu');
     };
-
     const eventListenerAnimation = () => {
       document.addEventListener('click', event => {
         const target = event.target,
           targetMenu = target.closest('.menu'),
           targetCloseBtn = target.closest('.close-btn'),
-          targetMenuItem = target.closest('ul>li');
-        if (targetMenu !== null) {
+          targetMenuItem = target.closest('ul>li>a');
+        if (targetMenu !== null && window.innerWidth < 768) {
           menu.style.transform = 'translateX(100%)';
-        } else if (targetCloseBtn !== null) {
+        } else if (targetCloseBtn !== null && window.innerWidth < 768) {
           menu.style.transform = 'translateX(-100%)';
-        } else if (targetMenuItem !== null) {
+        } else if (targetMenuItem !== null && window.innerWidth < 768) {
           menu.style.transform = 'translateX(-100%)';
         }
       });
     };
-
+    eventListenerAnimation();
     const eventListener = () => {
       document.addEventListener('click', event => {
         const target = event.target,
           targetMenu = target.closest('.menu'),
-          targetCloseBtn = target.closest('.close-btn');
-        if (targetMenu !== null) {
+          targetCloseBtn = target.closest('.close-btn'),
+          targetMenuItem = target.closest('ul>li>a');
+        if (targetMenu !== null && window.innerWidth >= 768) {
+          menu.removeAttribute('style');
           handlerMenu();
-        } else if (targetCloseBtn !== null) {
+        } else if ((targetCloseBtn !== null || targetMenuItem !== null) && window.innerWidth >= 768) {
           handlerMenu();
         }
       });
     };
-
-    const width = document.documentElement.offsetWidth;
-    if (width < 768) {
-      eventListenerAnimation();
-    } else {
-      eventListener();
-    }
+    eventListener();
   };
   toggleMenu();
 
@@ -97,34 +92,39 @@ window.addEventListener('DOMContentLoaded', () => {
   const togglePopUp = () => {
     const popUp = document.querySelector('.popup'),
       popUpContent = document.querySelector('.popup-content'),
+      service = document.querySelector('.service'),
       popUpBtn = document.querySelectorAll('.popup-btn');
+
     const openPopUp = () => {
       let count = 0;
       const height = document.documentElement.offsetHeight / 50;
-      const openPopUp = setInterval(() => {
+      let openPopUpAnimation;
+      const openPopUp = () => {
+        openPopUpAnimation = requestAnimationFrame(openPopUp);
         count += 5;
         if ((count - 5) < height) {
           popUpContent.style.top = count + 'px';
         } else if (count === height) {
-          clearInterval(openPopUp);
+          cancelAnimationFrame(openPopUp);
         }
-      }, 20);
+      };
+      openPopUpAnimation = requestAnimationFrame(openPopUp);
     };
-    const width = document.documentElement.offsetWidth;
-    if (width < 768) {
-      popUpBtn.forEach(item => {
-        item.addEventListener('click', () => {
-          popUp.style.display = 'block';
+
+    service.addEventListener('click', event => {
+      let target = event.target;
+      if (target.classList.contains('popup-btn')) {
+        popUpBtn.forEach(item => {
+          if (item === target && window.innerWidth < 768) {
+            popUp.style.display = 'block';
+          } else if ((item === target && window.innerWidth >= 768)) {
+            popUp.style.display = 'block';
+            openPopUp();
+          }
         });
-      });
-    } else {
-      popUpBtn.forEach(item => {
-        item.addEventListener('click', () => {
-          popUp.style.display = 'block';
-          openPopUp();
-        });
-      });
-    }
+      }
+    });
+
     popUp.addEventListener('click', event => {
       let target = event.target;
       if (target.classList.contains('popup-close')) {
@@ -136,7 +136,6 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       }
     });
-
   };
   togglePopUp();
 
@@ -169,13 +168,57 @@ window.addEventListener('DOMContentLoaded', () => {
         });
       }
     });
-
-
-
-
   };
   tabs();
+
+
 
 });
 
 
+  // Timer
+  // const countTimer = deadline => {
+  //   const timerHours = document.getElementById('timer-hours'),
+  //     timerMinutes = document.getElementById('timer-minutes'),
+  //     timerSeconds = document.getElementById('timer-seconds');
+
+  //   const getTimeRemaining = () => {
+  //     const dateStop = new Date(deadline).getTime(),
+  //       dateNow = new Date().getTime(),
+  //       timeRemaining = (dateStop - dateNow) / 1000,
+  //       seconds = Math.floor(timeRemaining % 60),
+  //       minutes = Math.floor((timeRemaining / 60) % 60),
+  //       hours = Math.floor(timeRemaining / 60 / 60);
+  //     const addZero = number => {
+  //       if (number < 10) {
+  //         number = '0' + number;
+  //         return number;
+  //       } else {
+  //         return number;
+  //       }
+  //     };
+  //     return {
+  //       timeRemaining,
+  //       hours: addZero(hours),
+  //       minutes: addZero(minutes),
+  //       seconds: addZero(seconds)
+  //     };
+  //   };
+  //   const updateClock = () => {
+  //     const timer = getTimeRemaining();
+  //     if (timer.timeRemaining > 0) {
+  //       timerHours.textContent = timer.hours;
+  //       timerMinutes.textContent = timer.minutes;
+  //       timerSeconds.textContent = timer.seconds;
+  //     }
+  //     if (timer.timeRemaining <= 0) {
+  //       clearInterval(updateClock);
+  //       timerHours.textContent = '00';
+  //       timerMinutes.textContent = '00';
+  //       timerSeconds.textContent = '00';
+  //     }
+  //   };
+  //   setInterval(updateClock, 1000);
+  //   updateClock();
+  // };
+  // countTimer('26 february 2021 20:52:10');
