de2413498aa2692934d9ab0502c19c0680e84c26
Shmakov Ilya <ilia.eduardovich98@gmail.com>
2021-03-02 15:24:37 +0300

    feat(script.js): create calc

--- script/script.js
+++ script/script.js
@@ -344,5 +344,52 @@ window.addEventListener('DOMContentLoaded', () => {
   validateNameAndMessage();
 
 
+  // Калькулятор
+
+  const calc = (price = 100) => {
+
+    const calcBlock = document.querySelector('.calc-block'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcCount = document.querySelector('.calc-count'),
+      calcDay = document.querySelector('.calc-day'),
+      calcTotal = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+
+      const typeValue = +calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      calcTotal.textContent = total;
+    };
+
+    calcBlock.addEventListener('change', event => {
+      const target = event.target;
+      if (target.matches('.calc-type') ||
+        target.matches('.calc-square') ||
+        target.matches('.calc-count') ||
+        target.matches('.calc-day')) {
+        countSum();
+      }
+    });
+  };
+  calc();
 
 });
